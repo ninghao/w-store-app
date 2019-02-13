f53b93a89d333e2d7ba0d1c4ab563b7e989396fc
wanghao8080 <117663444@qq.com>
2019-02-13 15:48:05 +0800

    微信小程序真机调试接口配置

--- config/dev.js
+++ config/dev.js
@@ -3,7 +3,7 @@ module.exports = {
     NODE_ENV: '"development"'
   },
   defineConstants: {
-    API_WS: '"http://localhost:3333"'
+    API_WS: '"https://w-store.ninghao.net"'
   },
   weapp: {},
   h5: {}

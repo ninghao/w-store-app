442d649df60df580e5207d251e3c78bee000351e
wanghao8080 <117663444@qq.com>
2019-02-15 19:52:08 +0800

    标签项目小徽章与小红点

--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -13,12 +13,45 @@ class ShopIndex extends Component {
     })
   }
 
+  setTabBarBadge(type) {
+    switch (type) {
+      case 'badge':
+        Taro.setTabBarBadge({
+          index: 1,
+          text: '1',
+        })
+        break;
+      case 'dot':
+        Taro.showTabBarRedDot({
+          index: 1,
+        })
+        break;
+    }
+  }
+
+  removeTabBarBadge(type) {
+    switch (type) {
+      case 'badge':
+        Taro.removeTabBarBadge({
+          index: 1,
+        })
+        break;
+      case 'dot':
+        Taro.hideTabBarRedDot({
+          index: 1,
+        })
+        break;
+    }
+  }
+
   render() {
     return (
       <View>
         <SearchBar />
         <View className='page-demo'>
           <Text className='mx-1' onClick={this.switchTab.bind(this)}>SwitchTab</Text>
+          <Text className='mx-1' onClick={this.setTabBarBadge.bind(this, 'dot')}>Add</Text>
+          <Text className='mx-1' onClick={this.removeTabBarBadge.bind(this, 'dot')}>Remove</Text>
         </View>
       </View>
     )

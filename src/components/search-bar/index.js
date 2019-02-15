4abe8aa6a6a1e97713a6da6290661a3d476b3acb
wanghao8080 <117663444@qq.com>
2019-02-15 18:41:13 +0800

    搜索栏的属性与事件

--- src/components/search-bar/index.js
+++ src/components/search-bar/index.js
@@ -3,10 +3,32 @@ import { View } from '@tarojs/components'
 import { AtSearchBar } from 'taro-ui'
 
 class SearchBar extends Component {
+  state = {
+    value: ''
+  }
+
+  onChange(value) {
+    this.setState({
+      value
+    })
+  }
+
+  onActionClick() {
+    console.log(`搜索：${this.state.value}`)
+  }
+
+  onConfirm() {
+    console.log(`搜索：${this.state.value}`)
+  }
+
   render() {
     return (
       <View>
-        <AtSearchBar />
+        <AtSearchBar
+          value={this.state.value}
+          onChange={this.onChange.bind(this)}
+          onActionClick={this.onActionClick.bind(this)}
+        />
       </View>
     )
   }

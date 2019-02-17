96f15e26a731c2bcba5dd40532e90da75d9bea96
wanghao8080 <117663444@qq.com>
2019-02-17 20:51:55 +0800

    产品列表组件（ProductList）

---
+++ src/components/product-list/index.js
@@ -0,0 +1,45 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Image, Text } from '@tarojs/components'
+import { AtBadge } from 'taro-ui'
+
+class ProductList extends Component {
+  static options = {
+    addGlobalClass: true
+  }
+
+  render() {
+    const { data: products } = this.props
+
+    return (
+      <View>
+        {
+          products.map(product =>
+            <View key={product.id} className='card mb-2'>
+              <Image
+                className='card-img-top'
+                src={product.images[0].src}
+                mode='aspectFit'
+              />
+              <View className='card-body text-center'>
+                <View className='card-title mb-2'>
+                  <View className='card-title-text'>
+                    {product.on_sale &&
+                      <AtBadge className='card-title-badge' value='sale' />}
+                    {product.name}
+                  </View>
+                </View>
+                <View className='card-subtitle'>
+                  {product.on_sale &&
+                    <Text className='mr-2 text-muted text-through'>{'￥' + product.regular_price}</Text>}
+                  <Text>{'￥' + product.price}</Text>
+                </View>
+              </View>
+            </View>
+          )
+        }
+      </View>
+    )
+  }
+}
+
+export default ProductList
--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -1,7 +1,7 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Image, Text } from '@tarojs/components'
-import { AtBadge } from 'taro-ui'
+import { View } from '@tarojs/components'
 import SearchBar from '../../components/search-bar'
+import ProductList from '../../components/product-list'
 
 class ShopIndex extends Component {
   config = {
@@ -28,33 +28,7 @@ class ShopIndex extends Component {
     return (
       <View>
         <SearchBar />
-        <View className='p-3'>
-          {
-            products.map(product =>
-              <View key={product.id} className='card mb-2'>
-                <Image
-                  className='card-img-top'
-                  src={product.images[0].src}
-                  mode='aspectFit'
-                />
-                <View className='card-body text-center'>
-                  <View className='card-title mb-2'>
-                    <View className='card-title-text'>
-                      {product.on_sale &&
-                        <AtBadge className='card-title-badge' value='sale' />}
-                      {product.name}
-                    </View>
-                  </View>
-                  <View className='card-subtitle'>
-                    {product.on_sale &&
-                      <Text className='mr-2 text-muted text-through'>{'￥' + product.regular_price}</Text>}
-                    <Text>{'￥' + product.price}</Text>
-                  </View>
-                </View>
-              </View>
-            )
-          }
-        </View>
+        <ProductList data={products} />
       </View>
     )
   }

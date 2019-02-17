718882f781a267f59165e6e0e87b5a833a7ae284
wanghao8080 <117663444@qq.com>
2019-02-17 20:26:41 +0800

    产品列表样式

--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -30,15 +30,17 @@ class ShopIndex extends Component {
         <View className='p-3'>
           {
             products.map(product =>
-              <View key={product.id}>
+              <View key={product.id} className='card mb-2'>
                 <Image
+                  className='card-img-top'
                   src={product.images[0].src}
                   mode='aspectFit'
                 />
-                <View>
-                  <View>{product.name}</View>
-                  <View>
-                    {product.on_sale && <Text className='mr-2'>{'￥' + product.regular_price}</Text>}
+                <View className='card-body text-center'>
+                  <View className='card-title mb-2'>{product.name}</View>
+                  <View className='card-subtitle'>
+                    {product.on_sale &&
+                      <Text className='mr-2 text-muted text-through'>{'￥' + product.regular_price}</Text>}
                     <Text>{'￥' + product.price}</Text>
                   </View>
                 </View>

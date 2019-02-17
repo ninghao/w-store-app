4e00d4e1bc2c18f541112bbc6108cc390e633d88
wanghao8080 <117663444@qq.com>
2019-02-17 19:30:10 +0800

    产品列表数据

--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -1,5 +1,5 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View } from '@tarojs/components'
 import SearchBar from '../../components/search-bar'
 
 class ShopIndex extends Component {
@@ -7,51 +7,32 @@ class ShopIndex extends Component {
     navigationBarTitleText: 'W-Store'
   }
 
-  switchTab() {
-    Taro.switchTab({
-      url: '/pages/shop/cart',
+  state = {
+    products: []
+  }
+
+  async componentWillMount() {
+    const response = await Taro.request({
+      url: `${API_WS}/products`
+    })
+
+    this.setState({
+      products: response.data
     })
   }
 
-  setTabBarBadge(type) {
-    switch (type) {
-      case 'badge':
-        Taro.setTabBarBadge({
-          index: 1,
-          text: '1',
-        })
-        break;
-      case 'dot':
-        Taro.showTabBarRedDot({
-          index: 1,
-        })
-        break;
-    }
-  }
-
-  removeTabBarBadge(type) {
-    switch (type) {
-      case 'badge':
-        Taro.removeTabBarBadge({
-          index: 1,
-        })
-        break;
-      case 'dot':
-        Taro.hideTabBarRedDot({
-          index: 1,
-        })
-        break;
-    }
-  }
-
   render() {
+    const { products } = this.state
+
     return (
       <View>
         <SearchBar />
-        <View className='page-demo'>
-          <Text className='mx-1' onClick={this.switchTab.bind(this)}>SwitchTab</Text>
-          <Text className='mx-1' onClick={this.setTabBarBadge.bind(this, 'dot')}>Add</Text>
-          <Text className='mx-1' onClick={this.removeTabBarBadge.bind(this, 'dot')}>Remove</Text>
+        <View className='p-3'>
+          {
+            products.map(product =>
+              <View key={product.id}>{product.name}</View>
+            )
+          }
         </View>
       </View>
     )

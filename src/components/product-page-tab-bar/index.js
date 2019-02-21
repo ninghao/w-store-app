2a4ed2647e7c93ac3caec8635a090ad507a3b08c
wanghao8080 <117663444@qq.com>
2019-02-21 20:45:02 +0800

    购物标签栏：样式

--- src/components/product-page-tab-bar/index.js
+++ src/components/product-page-tab-bar/index.js
@@ -11,20 +11,20 @@ class ProductPageTabBar extends Component {
   render() {
     return (
       <View className={this.props.className}>
-        <View className='at-tab-bar at-tab-bar--fixed'>
-          <View className='at-tab-bar__item'>
+        <View className='at-tab-bar at-tab-bar--fixed tab-bar'>
+          <View className='at-tab-bar__item tab-bar__item--icon'>
             <AtBadge dot>
               <View className='at-tab-bar__icon'>
                 <MaterialIcon icon='shopping_basket' color='#000' />
               </View>
             </AtBadge>
           </View>
-          <View>
+          <View className='tab-bar__item'>
             <AtButton
               type='secondary'
             >加入购物袋</AtButton>
           </View>
-          <View>
+          <View className='tab-bar__item'>
             <AtButton
               type='primary'
             >立即购买</AtButton>

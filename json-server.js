41d8f0a86dd0f34f064ba9f1a7c3788435de9415
wanghao8080 <117663444@qq.com>
2019-02-23 13:18:01 +0800

    读取与写入数据库文件（lowdb）

--- json-server.js
+++ json-server.js
@@ -1,8 +1,12 @@
 /* eslint-disable import/no-commonjs */
 const jsonServer = require('json-server')
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
 const path = require('path')
 
 const dbFile = path.join(__dirname, 'src', 'assets', 'db.json')
+const adapter = new FileSync(dbFile)
+const db = low(adapter)
 
 const server = jsonServer.create()
 const router = jsonServer.router(dbFile)

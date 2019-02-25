22e092328107065afc88b0a23fb829ad679c9395
wanghao8080 <117663444@qq.com>
2019-02-25 21:43:04 +0800

    获取购物车数据

--- src/pages/shop/cart.js
+++ src/pages/shop/cart.js
@@ -1,9 +1,44 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
+import fetchData from '../../utilities/fetch-data'
 
 class ShopCart extends Component {
   config = {
     navigationBarTitleText: '购物袋'
+  }
+
+  state = {
+    cart: {
+      total: 0,
+      items: []
+    }
+  }
+
+  constructor() {
+    this.fetchData = fetchData
+  }
+
+  getCartSuccess(response) {
+    this.setState({
+      cart: response.data
+    })
+
+    console.log(response.data)
+  }
+
+  getCart() {
+    this.fetchData({
+      resource: 'shopping-cart',
+      success: this.getCartSuccess.bind(this)
+    })
+  }
+
+  componentWillMount() {
+    this.getCart()
+  }
+
+  componentDidShow() {
+    this.getCart()
   }
 
   render() {

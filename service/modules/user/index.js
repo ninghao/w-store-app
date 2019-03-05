62fafd15cda6116ab54c30663b2789a9231e7122
wanghao8080 <117663444@qq.com>
2019-03-05 13:08:46 +0800

    用户注册接口（bcrypt hash 用户密码）

--- service/modules/user/index.js
+++ service/modules/user/index.js
@@ -1,7 +1,33 @@
 /* eslint-disable import/no-commonjs */
 const express = require('express')
+const bcrypt = require("bcrypt")
 const { db } = require('../../db')
 
 const router = express.Router()
+
+const getUserByName = (username) => {
+  const result = db.get('users')
+    .find({ username })
+    .value()
+
+  return result
+}
+
+// 用户注册
+router.post('/users', (req, res, next) => {
+  const { username, password } = req.body
+  const user = getUserByName(username)
+
+  if (user) {
+    res.status(409).jsonp('用户名被占用了！')
+    return
+  }
+
+  bcrypt.hash(password, 10)
+    .then(passwordHash => {
+      req.body.password = passwordHash
+      next()
+    })
+})
 
 module.exports = router

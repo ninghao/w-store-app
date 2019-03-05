864faa2b8adbb17e5ecbce261a66d989e999769f
wanghao8080 <117663444@qq.com>
2019-03-05 13:15:33 +0800

    用户登录接口

--- service/modules/user/index.js
+++ service/modules/user/index.js
@@ -30,4 +30,24 @@ router.post('/users', (req, res, next) => {
     })
 })
 
+// 用户登录
+router.post('/user-login', (req, res) => {
+  const { username, password } = req.body
+  const user = getUserByName(username)
+
+  if (!user) {
+    res.status(404).jsonp('用户名不存在。')
+    return
+  }
+
+  bcrypt.compare(password, user.password)
+    .then(result => {
+      if (result) {
+        res.jsonp('登录成功。')
+      } else {
+        res.status(401).jsonp('密码不匹配！')
+      }
+    })
+})
+
 module.exports = router

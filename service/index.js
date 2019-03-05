d52ebf55ddfc1a6eb277a6d83e176a3848f4d3b6
wanghao8080 <117663444@qq.com>
2019-03-05 15:37:43 +0800

    修改 Users 接口响应的资源（去掉密码字段）

--- service/index.js
+++ service/index.js
@@ -14,6 +14,34 @@ server.use(jsonServer.bodyParser)
 
 server.use([cart, user])
 
+router.render = (req, res) => {
+  if (req.method === 'GET') {
+    if (req.path === '/users') {
+      const users = res.locals.data.map(item => {
+        delete item.password
+
+        return item
+      })
+
+      res.jsonp(users)
+
+      return
+    }
+
+    const singleUser = req.path.match(/\/users\/(\d+)/)
+
+    if (singleUser) {
+      delete res.locals.data.password
+
+      res.jsonp(res.locals.data)
+
+      return
+    }
+  }
+
+  res.jsonp(res.locals.data)
+}
+
 server.use(router)
 
 server.listen(3333, () => {

a28d50c627ab1e091366d038fe1e8427446ed487
wanghao8080 <117663444@qq.com>
2019-03-06 10:45:10 +0800

    登录表单：处理登录

--- src/pages/user/account.js
+++ src/pages/user/account.js
@@ -1,6 +1,6 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
-import { AtInput, AtButton } from 'taro-ui'
+import { AtInput, AtButton, AtMessage } from 'taro-ui'
 
 class UserAccount extends Component {
   config = {
@@ -12,9 +12,61 @@ class UserAccount extends Component {
     password: ''
   }
 
+  async userLogin() {
+    const { username, password } = this.state
+
+    const response = await Taro.request({
+      method: 'POST',
+      url: `${API_WS}/user-login`,
+      data: {
+        username,
+        password
+      }
+    })
+
+    switch (response.statusCode) {
+      case 200:
+        await Taro.setStorage({
+          key: 'token',
+          data: response.data
+        })
+
+        break
+      case 404:
+        Taro.atMessage({
+          type: 'error',
+          message: response.data
+        })
+
+        break
+      case 401:
+        Taro.atMessage({
+          type: 'error',
+          message: response.data
+        })
+
+        break
+    }
+  }
+
+  handleChange(field, value) {
+    this.setState({
+      [field]: value
+    })
+  }
+
+  handleClick(field) {
+    switch (field) {
+      case 'login':
+        this.userLogin(this)
+        break
+    }
+  }
+
   render() {
     return (
       <View className='pt-5 m-5 form'>
+        <AtMessage />
         <AtInput
           name='username'
           type='text'

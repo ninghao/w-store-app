d7103ba75431458389465ec93705228955a62639
wanghao8080 <117663444@qq.com>
2019-03-06 10:10:53 +0800

    登录表单：视图

--- src/pages/user/account.js
+++ src/pages/user/account.js
@@ -1,15 +1,41 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
+import { AtInput, AtButton } from 'taro-ui'
 
 class UserAccount extends Component {
   config = {
-    navigationBarTitleText: 'UserAccount'
+    navigationBarTitleText: '登录'
+  }
+
+  state = {
+    username: '',
+    password: ''
   }
 
   render() {
     return (
-      <View>
-        UserAccount
+      <View className='pt-5 m-5 form'>
+        <AtInput
+          name='username'
+          type='text'
+          placeholder='用户'
+          value={this.state.username}
+          onChange={this.handleChange.bind(this, 'username')}
+          className='mb-3'
+        />
+        <AtInput
+          name='password'
+          type='password'
+          placeholder='密码'
+          value={this.state.password}
+          onChange={this.handleChange.bind(this, 'password')}
+          className='mb-3'
+        />
+        <AtButton
+          type='primary'
+          onClick={this.handleClick.bind(this, 'login')}
+          className='mt-5'
+        >登录</AtButton>
       </View>
     )
   }
